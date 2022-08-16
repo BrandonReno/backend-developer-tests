69b49a5ffc21b46e51aa4d1eded8f2ef9188c3c4
Brandon Reno <b_reno@u.pacific.edu>
2022-08-16 16:41:46 -0700

    Challenge-3: Began implementation

--- input-processing/main.go
+++ input-processing/main.go
@@ -4,14 +4,22 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	_ "strings"
 )
 
 func main() {
 	fmt.Println("SP// Backend Developer Test - Input Processing")
 	fmt.Println()
 
-	// Read STDIN into a new buffered reader
-	reader := bufio.NewReader(os.Stdin)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
 
 	// TODO: Look for lines in the STDIN reader that contain "error" and output them.
 }
